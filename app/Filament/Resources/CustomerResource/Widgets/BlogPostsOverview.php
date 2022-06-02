c5c2571da4576cea3524f510a273e354e3d03649
wandieinnocents <wandieinnocent2@gmail.com>
2022-06-02 12:18:49 +0300

    Add widgets

---
+++ app/Filament/Resources/CustomerResource/Widgets/BlogPostsOverview.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Resources\CustomerResource\Widgets;
+
+use Filament\Widgets\Widget;
+
+class BlogPostsOverview extends Widget
+{
+    protected static string $view = 'filament.resources.customer-resource.widgets.blog-posts-overview';
+}
